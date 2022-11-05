ed4fe38125edfc55fc3bb0bf7461a7a79f4269e0
ogiwelcome <ogi.fordevelopment@gmail.com>
2022-11-05 13:26:43 +0900

    add testcase for double colon

--- env_conf_test.go
+++ env_conf_test.go
@@ -10,12 +10,14 @@ func TestUpdate(t *testing.T) {
 	os.Setenv("ENV_ONE", "apples")
 	os.Setenv("ENV_TWO", "bananas")
 	os.Setenv("ENV_FOUR", "https://.google.com")
+	os.Setenv("ENV_FIVE", "https://github.com:my::value")
 
 	type Config struct {
 		EnvOne   string `env_conf:"ENV_ONE"`
 		EnvTwo   string `env_conf:"ENV_TWO:plums"`
 		EnvThree string `env_conf:"ENV_THREE:http://127.0.0.1:8080"`
 		EnvFour  string `env_conf:"ENV_FOUR:http://localhost:9090"`
+		EnvFive  string `env_conf:"ENV_FIVE:http://github.com:my::value"`
 	}
 
 	c := Config{}
@@ -44,6 +46,11 @@ func TestUpdate(t *testing.T) {
 	// Fixes bug - variable name is incorrect #5 (https://github.com/joegasewicz/env-conf/issues/5)
 	if c.EnvFour != "https://.google.com" {
 		t.Logf("expected https://.google.com but got %s", c.EnvFour)
+		t.Fail()
+	}
+
+	if c.EnvFive != "https://github.com:my::value" {
+		t.Logf("expected https://github.com:my::value but got %v", c.EnvFive)
 		t.Fail()
 	}
 
